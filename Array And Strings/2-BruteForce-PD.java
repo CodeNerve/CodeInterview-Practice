3b36c1930152a85d3d5fc6d0c10b66f41ad1b93b
Pradyuman Dixit <crvasu779@gmail.com>
2019-01-15 06:16:26 +0100

    Update 2-BruteForce-PD.java

--- Array And Strings/2-BruteForce-PD.java
+++ Array And Strings/2-BruteForce-PD.java
@@ -6,13 +6,11 @@ public static void permutation1(String pre, String s) {
 			str.add(pre);
 		
 		else {
-			
 			for(int i=0;i<n;i++)
 				permutation1(pre+s.charAt(i), s.substring(0, i) + s.substring(i+1,n));
-			
 		}
 		
-	}
+}
 	
 public static boolean checkPermutationB(String other, String s) {
 		
